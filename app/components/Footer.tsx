d2c8624493919592a93f4c71fd5e7cc52bf7ec62
Isreality <isrealitycreations@gmail.com>
2026-01-10 13:18:49 +0100

    worked on the services and contact section

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,28 +1,40 @@
 // import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import { Icon } from '@iconify/react';
 // import Ankra from "../img/Ankra.png";
 
 export default function Footer() {
   return (
-    <div>
-        <div className="bg-black py-16 px-4 md:px-8 lg:px-24 gap-10">
+    <div className="bg-black h-140 overflow-hidden gap-0">
+        <div className="pt-16 pb-0 px-4 md:px-8 lg:px-24">
             <div className="flex flex-row justify-between">
                 <div className="">
-                    <p className="text-white text-left text-sm md:text-base font-normal ">© 2025, ISRAEL AGBAJE (ISREALITY)</p>
+                    <p className="text-white text-left text-sm md:text-2xl font-normal ">© 2025, ISRAEL AGBAJE (ISREALITY)</p>
                 </div>
 
-                <div className="flex flex-row gap-5">
-                    <p className="text-white text-left text-sm md:text-base mb-4">Github</p>
-                    <p className="text-white text-left text-sm md:text-base mb-4">Behance</p>                   
-                    <p className="text-white text-left text-sm md:text-base mb-4">X</p>
-                    <p className="text-white text-left text-sm md:text-base mb-4">Linkedin</p>
+                <div className="flex flex-row gap-5 items-center">
+                    <Link href="https://www.github.com/Isreality">
+                        <Icon icon="qlementine-icons:github-24" className="text-white" width="40" height="40" />
+                    </Link>
+
+                    <Link href="https://www.behance.net/agbajeisrael">
+                        <Icon icon="cib:behance" className="text-white" width="40" height="40" />
+                    </Link>
+
+                    <Link href="https://www.x.com/Agbaje_Israel01">
+                        <Icon icon="prime:twitter" className="text-white" width="35" height="35" />
+                    </Link>
+
+                    <Link href="https://www.linkedin.com/in/agbajeisrael">
+                        <Icon icon="ri:linkedin-fill" className="text-white" width="40" height="40" />
+                    </Link>
                 </div>
             </div>
+        </div>
 
-            <div>
-                <p className="text-8xl text-white font-bold">ISREALITY</p>
-            </div>
+        <div className="">
+            <p className="text-[450px] text-[#1f1f1f] font-bold overflow-hidden">ISREALITY</p>
         </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import Navbar from "./components/Navbar"
+import { Icon } from '@iconify/react';
 import Btn from "./components/Btn";
 
 export default function Home() {
@@ -8,7 +10,7 @@ export default function Home() {
       <main>
         {/* Hero Section */}
         <div className="gap-0">
-            <div className="z-100"><Navbar/></div>
+            {/* <div className="z-100"><Navbar/></div> */}
 
             {/* Content */}
             <div className="flex flex-row items-center bg-white -mt-30">
@@ -46,6 +48,81 @@ export default function Home() {
               </div>
             </div>   
         </div>
+        
+        {/* SERVICES */}
+        <div className="bg-white w-full px-4 md:px-8 lg:px-24 py-16">
+          <h2 className="uppercase text-black text-center md:text-center lg:text-left font-black text-4xl md:text-6xl lg:text-7xl">
+            <b className="text-[#646464]">My</b> Expertise
+          </h2><br/>
+
+          {/* Features */}
+          <div>
+            {/* UI/UX */}
+            <Link href="/" className="bg-black flex flex-row rounded-lg gap-2 md:gap-1 px-4 md:px-8 py-16 border-b-2 items-center justify-between">
+                  <div className="flex flex-row uppercase text-white text-xl md:text-5xl w-[90%]">
+                      <h2 className="font-black">01.</h2>    
+                      <p className="font-light">UI/UX Design</p>
+                  </div>
+
+                  <div className="">
+                    <p className="uppercase text-sm md:text-2xl text-white w-[80%] font-light">Focus on creating visually appealing interfaces that delights users through wireframing, prototyping, and user testing to deliver a seamless user experience.</p>
+                  </div>
+
+                  <div className="bg-white p-2 rounded-full">
+                    <Icon className="text-black rotate-45 hover:rotate-90 cursor-pointer" icon="prime:arrow-up" width="50" height="50" />
+                  </div>  
+            </Link>
+
+            {/* Web */}
+            <Link href="/" className="bg-white hover:bg-[#fafafa] flex flex-row gap-2 md:gap-1 px-4 md:px-8 py-16 border-b-2 items-center justify-between">
+                  <div className="flex flex-row uppercase text-black text-xl md:text-5xl w-full">
+                      <h2 className="font-black">02.</h2>    
+                      <p className="font-light">WEB DEVELOPMENT</p>
+                  </div>
+
+                  <div className="">
+                    <p className="uppercase text-sm md:text-2xl text-black w-[80%] font-light">Create visually stunning and user-friendly websites and responsive web applications using modern technologies like HTML, CSS, JavaScript(React & Next Js) and Wordpress.</p>
+                  </div>
+
+                  <div className="bg-black p-2 rounded-full">
+                    <Icon className="text-white rotate-45 hover:rotate-90 cursor-pointer" icon="prime:arrow-up" width="50" height="50" />
+                  </div>  
+            </Link>
+
+            {/* Brand Design */}
+            <Link href="/" className="bg-white hover:bg-[#fafafa] flex flex-row gap-2 md:gap-1 px-4 md:px-8 py-16 border-b-2 items-center justify-between">
+                  <div className="flex flex-row uppercase text-black text-xl md:text-5xl w-[90%]">
+                      <h2 className="font-black">03.</h2>    
+                      <p className="font-light">Brand Design</p>
+                  </div>
+
+                  <div className="">
+                    <p className="uppercase text-sm md:text-2xl text-black w-[80%] font-light">Design unique logos and graphics that reflect the essence of your brand, ensuring it maintains a visually appealing image across all digital touchpoints.</p>
+                  </div>
+
+                  <div className="bg-black p-2 rounded-full">
+                    <Icon className="text-white rotate-45 hover:rotate-90 cursor-pointer" icon="prime:arrow-up" width="50" height="50" />
+                  </div>  
+            </Link>
+
+            {/* Illustration */}
+            <Link href="/" className="bg-white hover:bg-[#fafafa] flex flex-row gap-2 md:gap-1 px-4 md:px-8 py-16 border-b-2 items-center justify-between">
+                  <div className="flex flex-row uppercase text-black text-xl md:text-5xl w-full">
+                      <h2 className="font-black">04.</h2>    
+                      <p className="font-light">Illustration</p>
+                  </div>
+
+                  <div className="">
+                    <p className="uppercase text-sm md:text-2xl text-black w-[80%] font-light">Whether it’s digital artwork, character design, or icons, I create visuals that enhance storytelling and branding, bringing creativity and originality to every project.</p>
+                  </div>
+
+                  <div className="bg-black p-2 rounded-full">
+                    <Icon className="text-white rotate-45 hover:rotate-90 cursor-pointer" icon="prime:arrow-up" width="50" height="50" />
+                  </div>  
+            </Link>
+          </div>
+
+        </div>
 
         {/* CONTACT */}
         <div className="bg-white w-full px-4 md:px-8 lg:px-24 py-16">
@@ -72,20 +149,27 @@ export default function Home() {
             {/* Details */}
             <div className="grid grid-cols-2 items-center gap-5">
               <div className="bg-[#fafafa] flex flex-row gap-2 md:gap-5 px-4 md:px-8 py-16 rounded-lg items-center">
+                  <div className="flex flex-row bg-black px-6 py-6 rounded-full items-center gap-3">
+                      <Icon icon="ic:baseline-perm-phone-msg" className="text-white" width="40" height="40" />    
+                  </div>
+
                   <div className="">
-                  <h2 className="uppercase text-xl md:text-3xl text-black font-black mb-2">Phone</h2>
+                    <h2 className="uppercase text-xl md:text-3xl text-black font-black mb-2">Phone</h2>
                     <p className="uppercase text-sm md:text-3xl text-black font-light">+2348140082953</p>
                   </div>  
               </div>
 
               <div className="bg-[#fafafa] flex flex-row gap-2 md:gap-5 px-4 md:px-8 py-16 rounded-lg items-center">
+                  <div className="flex flex-row bg-black px-6 py-6 rounded-full items-center gap-3">
+                      <Icon icon="ic:baseline-email" className="text-white" width="40" height="40" />    
+                  </div>
+
                   <div className="">
-                  <h2 className="uppercase text-xl md:text-3xl text-black font-black mb-2">Email</h2>
+                    <h2 className="uppercase text-xl md:text-3xl text-black font-black mb-2">Email</h2>
                     <p className="text-sm md:text-3xl text-black font-light">isrealitycreations@gmail.com</p>
                   </div>  
               </div>
             </div>
-        
 
         </div>
 
