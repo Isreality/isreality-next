99a9a6eaa1d9be2ef7cba11d009a15b9ea7d4c24
Isreality <isrealitycreations@gmail.com>
2026-01-12 16:43:20 +0100

    Navbar

--- app/components/Btn.tsx
+++ app/components/Btn.tsx
@@ -4,9 +4,9 @@ import { Icon } from '@iconify/react';
 export default function Btn({name}: { name: string }) {
   return (
     // <div className="bg-primary hover:bg-black lg:w-32 py-6 px-20 rounded-md border-fa  cursor-pointer text-white text-xs md:text-base font-medium">{name}</div>
-    <Link href='/' className="flex flex-row bg-black px-6 py-4 rounded-full items-center gap-3">
+    <Link href='/' className="flex flex-row bg-black px-6 py-4 rounded-full items-center gap-3 justify-center">
         <p className="text-white text-xl uppercase">{name}</p>
-            <div className="bg-white p-2 rounded-full"><Icon className="text-black rotate-45 hover:rotate-90" icon="prime:arrow-up" width="20" height="20" /></div>
+        <div className="bg-white p-2 rounded-full"><Icon className="text-black rotate-45 hover:rotate-90" icon="prime:arrow-up" width="20" height="20" /></div>
     </Link>
   )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -19,10 +19,57 @@ export default function Navbar() {
 
   return (
     <div>
+      {/* Mobile */}
+      <div className="lg:hidden flex flex-row justify-between bg-white border-b-primary border-1 items-center px-4 py-4 md:px-8 md:py-6 left-0 w-full">
+        <div className="lg:hidden items-center">
+          <Image src="/img/dnt.jpg" alt="Dante" className="rounded-full" width={40} height={40}/>
+        </div>
+
+        <div className="lg:hidden z-50">    
+            <button onClick={()=>toggleNavbar()} className="text-black justify-center">{isOpen ? <Icon icon="material-symbols:close" className="fixed text-black font-bold size-5 top-5 z-50 mr-4 right-4 cursor-pointer" width="30" height="30" /> : <Icon icon="ci:hamburger-md" className="text-primary font-bold cursor-pointer" width="30" height="30" />}</button>
+              {isOpen &&(
+                <div>
+                  <div className="fixed h-full w-screen right-0 -translate-y-0 transition-all top-0 py-4">
+                    <div className="flex bg-white flex-col absolute inset-x-0 top-0 h-80 p-8 gap-5 z-[100] w-full">
+                      <nav className="flex flex-col gap-5 font-medium">
+                        <Link
+                          href="/"
+                          className={`${
+                            pathname === '/' ? 'text-black font-semibold text-xl text-center' : 'text-[#646464] hover:text-primary text-xl text-center'
+                          }`}
+                        >
+                          HOME
+                        </Link>
+                        <Link
+                          href="/about"
+                          className={`${
+                            pathname === '/about' ? 'text-black font-semibold text-xl text-center' : 'text-[#646464] hover:text-primary text-xl text-center'
+                          }`}
+                        >
+                          ABOUT 
+                        </Link>
+                        <Link
+                          href="/works"
+                          className={`${
+                            pathname === '/works' ? 'text-black font-semibold text-xl text-center' : 'text-[#646464] hover:text-primary text-xl text-center'
+                          }`}
+                        >
+                          MY WORKS
+                        </Link>
+                      </nav>
+
+                      <Btn name="Hire Me"/>
+
+                    </div> 
+                  </div>
+                </div>
+              )}      
+          </div>
+      </div>
       
 
       {/* Tab & Desktop */}
-      <div className="hidden md:hidden lg:flex lg:flex-wrap top-2 left-4 md:left-60 bg-none md:bg-none justify-between lg:px-24 md:px-8 py-4 m-0 z-50 items-center">
+      <div className="hidden md:hidden lg:flex lg:flex-wrap top-2 left-4 md:left-60 bg-none md:bg-none justify-between lg:px-24 md:px-16 py-4 m-0 z-50 items-center">
         <div className="hidden md:hidden lg:flex">
           <Image src="/img/dnt.jpg" alt="Dante" className="rounded-full" width={60} height={60}/>
         </div>
